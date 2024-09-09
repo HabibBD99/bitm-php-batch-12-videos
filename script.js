34d5af9389191dcc1b47e830c8b6f3f1fa8073db
HabibBD99 <habibsparknet@gmail.com>
2024-09-09 16:21:41 +0600

    day 37added

---
+++ script.js
@@ -0,0 +1,101 @@
+
+
+/*
+ const courseDetails = [
+    {
+         headline : "Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+         video :  "./day-2/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+         description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos"
+    },
+    {
+        headline : "Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        video :  "./day-2/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos"
+    },
+    {
+        headline : "Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        video :  "./day-2/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos"
+    },
+    {
+        headline : "Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        video :  "./day-2/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos"
+    },
+    {
+        headline : "Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        video :  "./day-2/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+        description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos"
+    }
+ ];
+
+ for(var i=0; i<courseDetails; i++){
+     var video = document.createElement("");
+     document.write(courseDetails[i].video);
+
+ }
+
+
+let videos = [
+    "./day-2/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 2.mp4",
+    "./day-3/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 3.mp4",
+    "./day-4/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 4.mp4",
+    "./day-5/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 5.mp4",
+    "./day-6/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 6.mp4",
+    "./day-7/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 7.mp4",
+    "./day-8/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 8.mp4",
+    "./day-9/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 9.mp4",
+    "./day-10/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 10.mp4",
+    "./day-11/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 11.mp4",
+    "./day-12/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 12.mp4",
+    "./day-13/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 13.mp4",
+    "./day-14/Web Design & Development Using PHP Laravel & JavaScript VueJS  Day 14.mp4"
+];
+
+
+
+
+
+
+for(videos of contents) {
+document.write(videos[i]);
+document.write("<br>");
+
+}
+*/
+
+/*
+const Video = document.getElementById('vid');
+
+for(var i=0; i<videos.length; i++)
+{
+video = document.createElement("video");
+video.src = videos[i];
+Video.appendChild(video);
+
+} 
+
+*/
+
+
+/*
+var showVideo = document.getElementById("card");
+for(var i=0; i<=videos.length; i++){
+        video = document.createElement("video");
+        video.src = videos[i];
+        showVideo.appendChild(video);
+
+};
+
+
+const container = document.getElementById('image-container');
+
+for (let i = 0; i < images.length; i++) {
+ const video = document.createElement('video');
+ video.src = images[i];
+ container.appendChild(img);
+}
+
+*/
+
+
